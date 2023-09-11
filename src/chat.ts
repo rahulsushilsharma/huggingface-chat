d19337233e6d87773a5a8e7767b57ed97e7d9850
Rahul Sharma <rahu8299@gmail.com>
2023-09-11 10:17:12 +0530

    preserve chat context (#2)
    * testing to preserve context of chats
    
    * .
    
    * able to preserve conversion
    
    * cleaning files

--- src/chat.ts
+++ src/chat.ts
@@ -1,239 +1,221 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { open } from "fs/promises"
-import { v4 as uuidv4 } from 'uuid';
-import { Readable, Transform } from 'stream';
-import readline from 'readline';
-// const Readable = require('stream').Readable;
+import { randomUUID } from 'crypto';
 
 export default class ChatBot {
-    private cookie!: string
-    private model !: string
-    private currentConversionID !: string
-    private headers = {
-        'authority': 'huggingface.co',
-        'accept': '*/*',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/json',
-        'origin': 'https://huggingface.co',
-        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+  private cookie!: string
+  private currentConversionID !: string
+  private chatLength = 0
+  private models = ['codellama/CodeLlama-34b-Instruct-hf']
+
+  constructor(cookie?: string, path?: string) {
+    if (!cookie && !path) throw new Error('cookie or path of cookie required')
+    else if (cookie && path) throw new Error('both cookie and path given')
+    else if (cookie && !path) this.cookie = cookie
+    else this.readCookiesFromPath(path)
+  }
+
+  async getHcSession() {
+
+  }
+  async readCookiesFromPath(path: string | undefined) {
+    if (!path) throw new Error('cookie path undefined')
+    const file = await open(path);
+
+    for await (const line of file.readLines()) {
+      this.cookie += line
     }
-    private chatLength = 0
+  }
 
-    constructor(cookie?: string, path?: string) {
-        if (!cookie && !path) throw new Error('cookie or path of cookie required')
-        else if (cookie && path) throw new Error('both cookie and path given')
-        else if (cookie && !path) this.cookie = cookie
-        else this.readCookiesFromPath(path)
+  async getNewChat() {
+    const model = {
+      model: this.models[0]
+    }
+    let response = await fetch("https://huggingface.co/chat/conversation", {
+      "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "cookie": this.cookie,
+        "Referer": "https://huggingface.co/chat/",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      "body": JSON.stringify(model),
+      "method": "POST"
+    }).catch(error => { throw new Error('Failed to create new conversion' + error) });
+
+    const { conversationId } = await response.json().catch(error => { throw new Error('Unknown response ' + error) });
+    this.currentConversionID = conversationId;
+    if (response.status != 200) throw new Error('Failed to create new conversion' + response)
+
+    await this.preserveContext(true)
+    return conversationId
+  }
+
+  async checkConversionId() {
+    console.log('conversionID', this.currentConversionID)
+
+    if (!this.currentConversionID) {
+
+      this.currentConversionID = await this.getNewChat()
+      console.log('creating new conversion', this.currentConversionID)
 
     }
+  }
 
-    async readCookiesFromPath(path: string | undefined) {
-        if (!path) throw new Error('cookie path undefined')
-        const file = await open(path);
 
-        for await (const line of file.readLines()) {
-            this.cookie += line
-        }
+  async chat(
+    text: string,
+    currentConversionID?: string,
+    temperature: number = 0.1,
+    truncate: number = 1000,
+    max_new_tokens: number = 2048,
+    top_p: number = 0.95,
+    repetition_penalty: number = 1.2,
+    top_k: number = 50,
+    return_full_text: boolean = false,
+    stream: boolean = true,
+    use_cache: boolean = false,
+    is_retry: boolean = false
+  ) {
+
+    if (text == "")
+      throw new Error("the prompt can not be empty.")
+
+    if (!currentConversionID) await this.checkConversionId()
+    else this.currentConversionID = currentConversionID
+
+    const data = {
+      'inputs': text,
+      'parameters': {
+        'temperature': temperature,
+        'truncate': truncate,
+        'max_new_tokens': max_new_tokens,
+        'top_p': top_p,
+        'repetition_penalty': repetition_penalty,
+        'top_k': top_k,
+        'return_full_text': return_full_text,
+
+      },
+      'stream': stream,
+      'options': {
+        'id': randomUUID(),
+        "response_id": randomUUID(),
+        'is_retry': is_retry,
+        'use_cache': use_cache,
+        "web_search_id": ""
+      }
     }
+    const response = await fetch("https://huggingface.co/chat/conversation/" + this.currentConversionID + "", {
+      "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "cookie": this.cookie,
+        "Referer": "https://huggingface.co/chat/conversation/" + this.currentConversionID + "",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      "body": JSON.stringify(data),
+      "method": "POST"
+    });
+    const resText = await response.text()
+    console.log(resText)
 
-    async getNewSession() {
-        let response: AxiosResponse<any, any>
-        try {
-            response = await axios.post(
-                'https://huggingface.co/chat/conversation',
-                {
-                    'model': this.model || 'meta-llama/Llama-2-70b-chat-hf'
-                },
-                {
-                    headers: {
-                        ...this.headers,
-                        'referer': 'https://huggingface.co/chat',
-                        'cookie': this.cookie
-                    }
-
-                }
-            )
-        } catch (e) {
-            throw new Error('Failed to faitch' + e)
-        }
-        if (!response) return
-
-        if (response.status != 200) throw new Error('Failed to create new conversion' + response)
-        await this.preserveContext(true)
-        return response.data['conversationId']
-    }
-
-    async checkConversionId() {
-        console.log('conversionID', this.currentConversionID)
-
-        if (!this.currentConversionID) {
-
-            this.currentConversionID = await this.getNewSession()
-            console.log('creating new conversion', this.currentConversionID)
-
-        }
-    }
-
-    async chat(
-        text: string,
-        currentConversionID?: string,
-        temperature: number = 0.9,
-        top_p: number = 0.95,
-        repetition_penalty: number = 1.2,
-        top_k: number = 50,
-        truncate: number = 100,
-        watermark: boolean = false,
-        max_new_tokens: number = 100,
-        stop = ["</s>"],
-        return_full_text: boolean = false,
-        stream: boolean = false,
-        use_cache: boolean = false,
-        is_retry: boolean = false
-    ) {
-
-        if (text == "")
-            throw new Error("the prompt can not be empty.")
-
-        if (!currentConversionID) await this.checkConversionId()
-        else this.currentConversionID = currentConversionID
-
-
-        const data = {
-            'inputs': text,
-            'parameters': {
-                'temperature': temperature,
-                'truncate': truncate,
-                "watermark": watermark,
-                'max_new_tokens': max_new_tokens,
-                "stop": stop,
-                'top_p': top_p,
-                'repetition_penalty': repetition_penalty,
-                'top_k': top_k,
-                'return_full_text': return_full_text
-            },
-            'stream': stream,
-            'options': {
-                'id': uuidv4(),
-                'is_retry': is_retry,
-                'use_cache': use_cache,
-                'web_search_id': ''
-            }
-        }
-        let response: AxiosResponse<any, any>
-
-        response = await axios.post(
-            `https://huggingface.co/chat/conversation/${this.currentConversionID}`,
-            data,
-            {
-                headers: {
-                    ...this.headers,
-                    'referer': `https://huggingface.co/chat/conversation/${this.currentConversionID}`,
-                    'cookie': this.cookie
-                },
-                responseType: stream ? 'stream' : undefined
-            }
-        );
-        console.log(response);
-
-
-        // if (response.status != 200) throw new Error('Failed to chat' + response)
-
-
-        if (this.chatLength <= 0) {
-            await this.summarizeConversation()
-        }
-
-
-        this.chatLength += 1;
-
-        // const transformStream = new Transform({
-        //     transform(chunk, encoding, callback) {
-        //         const transformedData = chunk.toString().substring(5).trim();
-        //         this.push(transformedData); // Push the transformed data to the new stream
-        //         callback();
-        //     }
-        // });
-        // response.data.pipe(transformStream)
-        return { id: this.currentConversionID, data: response.data }
+    if (this.chatLength <= 0) {
+      await this.summarizeConversation()
     }
 
 
-    // Return a summary of the conversation.
+    this.chatLength += 1;
+    return { id: this.currentConversionID, data: resText }
+  }
 
-    async summarizeConversation(conversation_id?: string) {
+  async summarizeConversation(conversation_id?: string) {
 
-        if (conversation_id)
-            this.currentConversionID = conversation_id
-        let response: AxiosResponse<any, any>
+    if (!conversation_id) {
+      conversation_id = this.currentConversionID
+    }
+    const response = await fetch("https://huggingface.co/chat/conversation/" + conversation_id + "/summarize", {
+      "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "cookie": this.cookie,
+        "Referer": "https://huggingface.co/chat/conversation/" + conversation_id + "",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      "body": null,
+      "method": "POST"
+    }).catch(error => {
+      throw new Error("Unable to summarize chat " + error)
 
-        try {
-            response = await axios.post(
-                `https://huggingface.co/chat/conversation/${this.currentConversionID}/summarize`,
-                '',
-                {
-                    headers: {
-                        ...this.headers,
-                        'Referer': `https://huggingface.co/chat/conversation/${this.currentConversionID}`,
-                        'cookie': this.cookie
-                    }
-                }
-            );
+    })
+    const resJson = await response.json().catch(error => { throw new Error('Unknown response ' + error) })
+    console.log('chat summary ', resJson)
+    return resJson
+  }
 
-        } catch (error) {
-            throw new Error("Unable to summarize chat " + error)
-        }
-        // if (response.data) {
-        //     try {
-        //         const res = JSON.parse(response.data)
-        //         if (!res.hasOwnProperty('title')) throw new Error("Unknown response from chat summarization " + response.data)
-        //     } catch (error) {
-        //         throw new Error("Unknown response from chat summarization " + response.data)
-        //     }
-        // }
-        console.log("summarizing chat", response.data);
 
-        return response.data
+  async preserveContext(newChat: boolean) {
+    let response: Response
+    if (newChat) {
+      response = await fetch("https://huggingface.co/chat/conversation/" + this.currentConversionID + "/__data.json?x-sveltekit-invalidated=1_1", {
+        "headers": {
+          "accept": "*/*",
+          "accept-language": "en-US,en;q=0.9",
+          "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "cookie": this.cookie,
+          "Referer": "https://huggingface.co/chat/",
+          "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+      });
+
+    } else {
+      response = await fetch("https://huggingface.co/chat/conversation/" + this.currentConversionID + "/__data.json?x-sveltekit-invalidated=1_", {
+        "headers": {
+          "accept": "*/*",
+          "accept-language": "en-US,en;q=0.9",
+          "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "cookie": this.cookie,
+          "Referer": "https://huggingface.co/chat/conversation/" + this.currentConversionID + "",
+          "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+      });
     }
 
+    if (response.status != 200) throw new Error("Unable to preserve chat context " + response)
 
-    async preserveContext(newChat: boolean) {
-        let response: AxiosResponse<any, any>
-        let referer = ''
-        let sveltekit = ''
-        if (newChat) {
-            referer = "https://huggingface.co/chat"
-            sveltekit = '1_1'
-        } else {
-            referer = `https://huggingface.co/chat/conversation/${this.currentConversionID}`
-            sveltekit = '1_'
-        }
-        try {
-            response = await axios.get(`https://huggingface.co/chat/conversation/${this.currentConversionID}/__data.json`, {
-                params: {
-                    'x-sveltekit-invalidated': sveltekit
-                },
-
-                headers: {
-                    ...this.headers,
-                    'Referer': referer,
-                    'cookie': this.cookie
-                }
-            });
-
-        } catch (error) {
-            throw new Error("Unable to preserve chat context " + error)
-        }
-        console.log("preversing chat", response.data);
-
-        return response
-    }
-
-
+    return response
+  }
 }
 
