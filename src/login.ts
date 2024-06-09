3061da3c352d1c7d7fbbea0cb1e65531daebad03
Rahul Sharma <rahu8299@gmail.com>
2024-06-09 18:40:09 +0530

    Update/cache miss message (#44)
    * adding extra messege to reduce confusion
    
    * typo fix

--- src/login.ts
+++ src/login.ts
@@ -298,6 +298,7 @@ export default class Login {
             return lines.join('');
         } catch (error) {
             console.error(`Error loading cache:`, error);
+            console.error("\nCreating new session.")
             return ''
         }
     }
