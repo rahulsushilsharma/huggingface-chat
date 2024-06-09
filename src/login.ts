0d8ee7d71abc55da31e42558d2ce86b8c8ed75aa
Rahul Sharma <rahu8299@gmail.com>
2024-06-09 18:35:51 +0530

    adding extra messege to reduce confusion

--- src/login.ts
+++ src/login.ts
@@ -297,6 +297,7 @@ export default class Login {
             return lines.join('');
         } catch (error) {
             console.error(`Error loading cache:`, error);
+            console.error("\ncreating new sesson")
             return ''
         }
     }
